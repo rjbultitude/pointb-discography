2dbc4d9e6589f21bb4e3769813cfb937b4b24ed4
Richard Bultitude <rbultitude@zonecontent.com>
2014-02-05 12:26:17 +0000

    structure data work - sort out newData object

--- site/src/scripts/modules/structure-data.js
+++ site/src/scripts/modules/structure-data.js
@@ -1,11 +1,11 @@
 /**
  * Example module module
  *
- * $author       Zone dev
- * $email        frontend@thisiszone.com
- * $url          http://www.thisiszone.com/
- * $copyright    Copyright (c) 2013, thisiszone.com. All rights reserved.
- * $version      1.0
+ * $author       Richard Bultitude
+ * $email        richard.bultitude@gmail.com
+ * $url          http://www.point-b.co.uk
+ * $copyright    Copyright (c) 2014, www.point-b.co.uk. All rights reserved.
+ * $version      1.1
  *
  * $notes        Notes
  */
@@ -20,11 +20,36 @@ define(['debug', 'loadData', 'createDrawGraph'], function(debug, loadDataModule,
 		createNewData: function createNewDataFn(jsonData) {
 			var oldData = jsonData;
 			var newData = [];
-			//var firstItem = newData[0];
+			var uniqueYears = [];
 
-			for (var i in oldData) {
-				newData.push(i.year);
+			//get unique years
+			for (var i = 0; i < oldData.length; i++) {
+				if (uniqueYears.indexOf(oldData[i].Year) === -1) {
+					uniqueYears.push(oldData[i].Year);
+				}
 			}
+			uniqueYears.sort();
+			debug.log('uniqueYears', uniqueYears);
+
+			//Create object for each year and add to newData
+			for (var j=0; j < uniqueYears.length; j++) {
+				var yearObject = {
+					year: uniqueYears[j],
+					releases: []
+				};
+				newData.push(yearObject);
+			}
+
+			//add relevant releases to each year
+			for (var k = 0; k < newData.length; k++) {
+				for (var l = 0; l < oldData.length; l++) {
+					if (newData[k].year === oldData[l].Year) {
+						newData[k].releases.push(oldData[l]);
+					}
+				}
+			}
+
+			debug.log('newData', newData);
 
 			//swap this for newData when ready
 			createDrawGraph.getData(oldData);
